af9a9c063fcda56b70538e632fee5080b9fbdb79
Derek Kent <dak@dak.org>
2015-05-21 11:52:59 -0400

    Add requestAnimationFrame shim

--- src/scripts/helpers/backbone/base.js
+++ src/scripts/helpers/backbone/base.js
@@ -2,6 +2,32 @@ import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
 
+// requestAnimationFrame Shim for browsers (and PhantomJS)
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 function dispose(obj) {
     delete obj.parent;
     delete obj.el;
@@ -76,7 +102,7 @@ class BaseView extends Backbone.View {
 
         this.template = options.template;
         this.regions = new Regions(options.regions, this);
-        
+
         this.initialize.apply(this, arguments);
     }
 
