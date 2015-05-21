2dab196ca78312254377f08e4b2b5226f092dac9
Derek Kent <dak@dak.org>
2015-05-21 17:21:25 -0400

    Separate src, dev build, and dist builds

--- gulpfile.js
+++ gulpfile.js
@@ -12,42 +12,56 @@ var handlebars = require('gulp-handlebars');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 
 var paths = {
-    html: 'src/index.html',
-    fonts: 'src/fonts/**/*',
-    text: 'src/robots.txt',
-    scripts: 'src/scripts/**/*.js',
-    styles: 'src/**/*.less',
-    templates: 'src/scripts/**/*.hbs',
-    partials: 'src/scripts/**/*.part.hbs',
-    images: 'src/images/**/*'
+    src: {
+        html: 'src/index.html',
+        fonts: 'src/fonts/**/*',
+        text: 'src/robots.txt',
+        scripts: 'src/scripts/**/*.js',
+        styles: 'src/**/*.less',
+        templates: 'src/scripts/**/*.hbs',
+        partials: 'src/scripts/**/*.part.hbs',
+        images: 'src/images/**/*'
+    },
+
+    dev: {
+        html: 'dev/index.html',
+        fonts: 'dev/fonts/**/*',
+        text: 'dev/robots.txt',
+        scripts: 'dev/scripts/**/*.js',
+        styles: 'dev/**/*.less',
+        templates: 'dev/scripts/**/*.hbs',
+        partials: 'dev/scripts/**/*.part.hbs',
+        images: 'dev/images/**/*'
+    },
+
+    jspm: 'jspm_packages/**/*.js'
 };
 
 /*
- * Dev Build Tasks
+ * Shared Functions
  *
  *
 */
 
-gulp.task('styles-dev', function () {
-    return gulp.src(paths.styles)
+function stylesDev () {
+    return gulp.src(paths.src.styles)
         .pipe(clip())
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./src/'));
-});
+        .pipe(gulp.dest('./dev/'));
+}
 
-gulp.task('templates', function () {
+function templates () {
     var header = 'import Handlebars from \'handlebars\';';
     var tmplHeader = header += 'export var template = Handlebars.template(<%= contents %>)';
     var partialHeader = header += ';export var partial = Handlebars' +
         '.registerPartial(<%= pro(file.relative) %>, Handlebars.template(<%= contents %>));'
 
-    gulp.src(paths.partials)
+    gulp.src(paths.src.partials)
         .pipe(handlebars())
         .pipe(wrap(partialHeader, {}, {
             imports: {
@@ -59,16 +73,43 @@ gulp.task('templates', function () {
         .pipe(rename(function (path) {
             path.extname = '.hbs.js'
         }))
-        .pipe(gulp.dest('./src/scripts/'));
+        .pipe(gulp.dest('./dev/scripts/'));
 
-    return gulp.src([paths.templates, '!'+paths.partials])
+    return gulp.src([paths.src.templates, '!'+paths.src.partials])
         .pipe(handlebars())
         .pipe(wrap(tmplHeader))
         .pipe(rename(function (path) {
             path.extname = '.hbs.js'
         }))
-        .pipe(gulp.dest('./src/scripts/'));
+        .pipe(gulp.dest('./dev/scripts/'));
+}
+
+/*
+ * Watch Build Tasks
+ *
+ *
+*/
+
+gulp.task('styles:watch', stylesDev);
+gulp.task('templates:watch', templates);
+
+/*
+ * Dev Build Tasks
+ *
+ *
+*/
+
+gulp.task('clean:dev', function (cb) {
+    del(['dev'], cb);
 });
+
+gulp.task('copy:dev', ['clean:dev'], function () {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('./dev/'));
+});
+
+gulp.task('styles:dev', ['copy:dev'], stylesDev);
+gulp.task('templates:dev', ['copy:dev'], templates);
 
 /*
  * Production Build Tasks
@@ -76,46 +117,45 @@ gulp.task('templates', function () {
  *
 */
 
-gulp.task('clean', function (cb) {
+gulp.task('clean:dist', function (cb) {
     del(['dist'], cb);
 });
 
-gulp.task('fonts', ['clean'], function () {
-    return gulp.src(paths.fonts)
+gulp.task('fonts', ['clean:dist', 'copy:dev'], function () {
+    return gulp.src(paths.dev.fonts)
         .pipe(gulp.dest('./dist/fonts/'));
 });
 
-gulp.task('html', ['clean'], function () {
-    return gulp.src(paths.html)
+gulp.task('html', ['clean:dist', 'copy:dev'], function () {
+    return gulp.src(paths.dev.html)
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('text', ['clean'], function () {
-    return gulp.src(paths.text)
+gulp.task('text', ['clean:dist', 'copy:dev'], function () {
+    return gulp.src(paths.dev.text)
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('scripts', ['clean', 'templates'], function () {
-    gulp.src(['jspm_packages/**/*.js'])
+gulp.task('scripts', ['clean:dist', 'templates:dev'], function () {
+    gulp.src([paths.jspm])
         .pipe(uglify())
         .pipe(gulp.dest('./dist/jspm_packages/'));
 
-    return gulp.src([paths.scripts])
+    return gulp.src([paths.dev.scripts])
         .pipe(babel())
         .pipe(uglify())
-        //.pipe(concat('main.js'))
         .pipe(gulp.dest('./dist/scripts/'));
 });
 
-gulp.task('images', ['clean'], function () {
-    return gulp.src(paths.images)
+gulp.task('images', ['clean:dist', 'copy:dev'], function () {
+    return gulp.src(paths.dev.images)
         .pipe(imagemin({optimizationLevel: 7}))
         .pipe(gulp.dest('./dist/images/'));
 });
 
-gulp.task('styles-dist', ['clean'], function () {
-    return gulp.src(paths.styles)
+gulp.task('styles:dist', ['clean:dist', 'styles:dev'], function () {
+    return gulp.src(paths.dev.styles)
         .pipe(clip())
         .pipe(less())
         .pipe(uncss({html: ['http://localhost:9912/?_escaped_fragment_=']}))
@@ -130,12 +170,13 @@ gulp.task('styles-dist', ['clean'], function () {
 */
 
 gulp.task('watch', ['dev'], function () {
-    gulp.watch(paths.templates, ['templates']);
-    gulp.watch(paths.styles, ['styles-dev']);
+    gulp.watch(paths.src.templates, ['templates:watch']);
+    gulp.watch(paths.src.styles, ['styles:watch']);
 });
 
-gulp.task('dev', ['templates', 'styles-dev']);
+gulp.task('dev', ['templates:dev', 'styles:dev']);
 
-gulp.task('dist', ['text', 'fonts', 'html', 'styles-dist', 'scripts', 'images']);
+gulp.task('dist', ['text', 'fonts', 'html', 'styles:dist', 'scripts', 'images']);
+gulp.task('build', ['dist']);
 
 gulp.task('default', ['dev']);
