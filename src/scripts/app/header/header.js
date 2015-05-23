3753e96ab7a9e8d3ec28a6c13fed980cc9886c10
Derek Kent <dak@dak.org>
2015-05-23 11:32:27 -0400

    Make header menu toggleable

--- src/scripts/app/header/header.js
+++ src/scripts/app/header/header.js
@@ -1,14 +1,42 @@
 import BaseView from '~/helpers/backbone/base';
 import { template } from './header.hbs';
-//System.import('~/app/header/header.css!');
-//System.import('./footer.css!', {name: __moduleName});
 
 export default class Header extends BaseView {
 
     constructor () {
         super({
             el: '#header',
-            template: template
+            template: template,
+
+            events: {
+                'click .expand-nav': 'toggleNavMenu'
+            }
         });
+    }
+
+    toggleNavMenu (e) {
+        let menu = this.el.querySelector('.menu');
+        let button = e.currentTarget;
+        let header = this.el.querySelector('.header');
+
+        /*if (menu.style.display === '') {
+            menu.style.display = 'block';
+        } else {
+            menu.style.display = '';
+        }*/
+
+        if (button.classList.contains('active')) {
+            button.classList.remove('active');
+        } else {
+            button.classList.add('active');
+        }
+
+        // TODO: this transition doesn't work smoothly, may need to do it in JS
+        //  Check to see how Bootstrap does its transition
+        if (header.style.maxHeight !== '100vh') {
+            header.style.maxHeight = '100vh';
+        } else {
+            header.style.maxHeight = '80px';
+        }
     }
 }
