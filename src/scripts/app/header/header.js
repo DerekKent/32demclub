2d4ad5a38af96500d8927d8cea81bc4a319ded13
Derek Kent <dak@dak.org>
2015-05-23 14:57:42 -0400

    Move Join to Home and cleanup header animation

--- src/scripts/app/header/header.js
+++ src/scripts/app/header/header.js
@@ -14,29 +14,26 @@ export default class Header extends BaseView {
         });
     }
 
+    onRender () {
+        if (this.el) {
+            this.el.querySelector('.header').style.maxHeight = '80px';
+        }
+    }
+
     toggleNavMenu (e) {
-        let menu = this.el.querySelector('.menu');
         let button = e.currentTarget;
         let header = this.el.querySelector('.header');
 
-        /*if (menu.style.display === '') {
-            menu.style.display = 'block';
+        if (header.style.maxHeight !== '80px') {
+            header.style.maxHeight = '80px';
         } else {
-            menu.style.display = '';
-        }*/
+            header.style.maxHeight = '';
+        }
 
         if (button.classList.contains('active')) {
             button.classList.remove('active');
         } else {
             button.classList.add('active');
-        }
-
-        // TODO: this transition doesn't work smoothly, may need to do it in JS
-        //  Check to see how Bootstrap does its transition
-        if (header.style.maxHeight !== '100vh') {
-            header.style.maxHeight = '100vh';
-        } else {
-            header.style.maxHeight = '80px';
         }
     }
 }
