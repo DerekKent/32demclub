e8e2ed9a9baed5c06954afd1a456fba154e4b9e0
Derek Kent <dak@dak.org>
2015-05-25 01:11:02 -0400

    Remove bootstrap

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -14,16 +14,12 @@ System.config({
     "babel": "npm:babel-core@5.4.3",
     "babel-runtime": "npm:babel-runtime@5.4.3",
     "backbone": "npm:backbone@1.2.0",
-    "bootstrap": "github:twbs/bootstrap@3.3.4",
     "core-js": "npm:core-js@0.9.11",
     "handlebars": "github:components/handlebars.js@3.0.3",
     "jquery": "github:components/jquery@2.1.4",
     "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
-    },
-    "github:twbs/bootstrap@3.3.4": {
-      "jquery": "github:components/jquery@2.1.4"
     },
     "npm:backbone@1.2.0": {
       "process": "github:jspm/nodelibs-process@0.1.1",
@@ -36,3 +32,4 @@ System.config({
     }
   }
 });
+
