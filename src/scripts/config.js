8a6eaf5bffe35858f5823159867621a9abe408bc
Derek Kent <dak@dak.org>
2015-07-11 01:14:26 -0400

    Upgrade to jspm 0.16 and remove transpiler on load

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,81 +1,18 @@
 System.config({
   "baseURL": "/",
-  "transpiler": "babel",
-  "babelOptions": {
-    "optional": [
-      "runtime"
-    ]
-  },
+  "defaultJSExtensions": true,
+  "transpiler": "none",
   "paths": {
-    "*": "*.js",
     "~/*": "scripts/*.js",
-    "github:*": "../jspm_packages/github/*.js",
-    "npm:*": "../jspm_packages/npm/*.js"
-  }
-});
-
-System.config({
-  "depCache": {
-    "github:akre54/Backbone.NativeView@0.3.3/backbone.nativeview": [
-      "npm:backbone@1.2.1"
-    ],
-    "npm:underscore@1.8.3": [
-      "npm:underscore@1.8.3/underscore"
-    ],
-    "npm:process@0.10.1": [
-      "npm:process@0.10.1/browser"
-    ],
-    "github:akre54/Backbone.NativeView@0.3.3": [
-      "github:akre54/Backbone.NativeView@0.3.3/backbone.nativeview"
-    ],
-    "github:components/handlebars.js@3.0.3": [
-      "github:components/handlebars.js@3.0.3/handlebars"
-    ],
-    "github:jspm/nodelibs-process@0.1.1/index": [
-      "npm:process@0.10.1"
-    ],
-    "~/helpers/backbone/base": [
-      "npm:underscore@1.8.3",
-      "npm:backbone@1.2.1",
-      "github:akre54/Backbone.NativeView@0.3.3"
-    ],
-    "~/app/app.hbs": [
-      "github:components/handlebars.js@3.0.3"
-    ],
-    "github:jspm/nodelibs-process@0.1.1": [
-      "github:jspm/nodelibs-process@0.1.1/index"
-    ],
-    "~/app/app": [
-      "npm:backbone@1.2.1",
-      "~/helpers/backbone/base",
-      "~/app/app.hbs"
-    ],
-    "npm:backbone@1.2.1/backbone": [
-      "npm:underscore@1.8.3",
-      "github:jspm/nodelibs-process@0.1.1"
-    ],
-    "~/router": [
-      "npm:backbone@1.2.1",
-      "~/app/app"
-    ],
-    "npm:backbone@1.2.1": [
-      "npm:backbone@1.2.1/backbone"
-    ],
-    "scripts/main": [
-      "npm:backbone@1.2.1",
-      "~/router",
-      "~/app/app"
-    ]
+    "github:*": "../jspm_packages/github/*",
+    "npm:*": "../jspm_packages/npm/*"
   }
 });
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.4.3",
-    "babel-runtime": "npm:babel-runtime@5.4.3",
     "backbone": "npm:backbone@1.2.1",
     "backbone.nativeview": "github:akre54/Backbone.NativeView@0.3.3",
-    "core-js": "npm:core-js@0.9.11",
     "handlebars": "github:components/handlebars.js@3.0.3",
     "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
@@ -84,12 +21,6 @@ System.config({
     "npm:backbone@1.2.1": {
       "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.8.3"
-    },
-    "npm:core-js@0.9.11": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
-
