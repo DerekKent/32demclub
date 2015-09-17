71edccaaabaca986db7c0d082b63546db70010db
Derek Kent <dak@dak.org>
2015-09-17 14:40:32 -0400

    Upgrade to Backbone 1.2.3

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,27 +1,24 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "none",
-  "paths": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "none",
+  paths: {
     "~/*": "scripts/*.js",
     "github:*": "../jspm_packages/github/*",
     "npm:*": "../jspm_packages/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
-    "backbone": "npm:backbone@1.2.1",
+  map: {
+    "backbone": "npm:backbone@1.2.3",
     "backbone.nativeview": "github:akre54/Backbone.NativeView@0.3.3",
     "handlebars": "github:components/handlebars.js@3.0.3",
     "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:backbone@1.2.1": {
+    "npm:backbone@1.2.3": {
       "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.8.3"
     }
   }
 });
-
